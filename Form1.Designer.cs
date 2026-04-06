2a47a28b8b9b5002526fca267fda6bf0c1227142
tejas <tejasspatil2601@gmai.com>
2026-04-06 16:10:07 +0530

    Calculator WinForms project

---
+++ Form1.Designer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CALCULATOR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Calculator";
+        }
+
+        #endregion
+    }
+}
